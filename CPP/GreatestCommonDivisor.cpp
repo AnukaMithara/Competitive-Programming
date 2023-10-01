2bf55224207fb6ae3b3e441d0be0cd443f905c33
AnukaMithara <anukamithara@gmail.com>
2023-10-01 21:53:22 +0530

    GCD

--- CPP/GreatestCommonDivisor.cpp
+++ CPP/GreatestCommonDivisor.cpp
@@ -2,9 +2,40 @@
 
 using namespace std;
 
+
+int gcd(int a, int b) {
+    if(b == 0)
+        return a;
+    else
+        return gcd(b, a%b);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;   
+    cout << gcd(a, b); 
+    return 0;
+}
+
+
+
+/*
+#include <iostream>
+
+using namespace std;
+
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << a + b;
+    
+    int min = a < b ? a : b;
+
+    for (int i = min; i >= 1; i--) {
+        if (a % i == 0 && b % i == 0) {
+            cout << i;
+            break;
+        }
+    }    
     return 0;
 }
+*/
--- Java/GreatestCommonDivisor.java
+++ Java/GreatestCommonDivisor.java
@@ -1,14 +1,40 @@
-// Don't place your source in a package
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-// Please name your class Main
+class Main {
+  public static void main(String[] args) throws java.lang.Exception {
+    Scanner in = new Scanner(System.in);
+    int a = in.nextInt();
+    int b = in.nextInt();
+
+    int gcd = gcd(a, b);
+    System.out.println(gcd);
+  }
+
+  private static int gcd(int a, int b) {
+    if (b == 0) {
+      return a;
+    } else {
+      return gcd(b, a % b);
+    }
+  }
+}
+
+
+/* 
+import java.util.*;
 class Main {
 	public static void main (String[] args) throws java.lang.Exception {
 	    Scanner in = new Scanner(System.in);
 		int a = in.nextInt();
 		int b = in.nextInt();
-		System.out.print(a + b);
+		int min = Math.min(a,b);		
+		for(int i=min;i>=1;i--){
+		    if(a%i==0 && b%i==0){
+		        System.out.println(i);
+		        break;
+		    }
+		}
 	}
 }
+
+*/
