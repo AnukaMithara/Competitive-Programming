5d39b1176a4e4610fb57b3e3b25d1e1debd8cd4d
AnukaMithara <anukamithara@gmail.com>
2023-10-02 22:15:31 +0530

    SortPartition and Word Ordering

---
+++ Java/SortingPartition.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) throws java.lang.Exception {
+    Scanner in = new Scanner(System.in);
+    int a = in.nextInt();
+    Integer[] b = new Integer[a];
+
+    for(int i=0; i<a; i++)
+    {
+        b[i] = in.nextInt();
+    }
+
+    
+
+    
+
+
+
+    System.out.println(a);
+  }
+
+ 
+}
---
+++ Java/WordOrdering.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) throws java.lang.Exception {
+    Scanner in = new Scanner(System.in);
+    String a = in.nextLine();
+    int b = in.nextInt();
+    String[] c = new String[b];
+
+    for(int i=0; i<b; i++)
+    {
+        c[i] = in.nextLine();
+    }
+
+    
+
+
+
+    System.out.println(a);
+  }
+
+ 
+}
